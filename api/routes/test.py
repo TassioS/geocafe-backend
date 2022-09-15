a3b979de8c43eddcc976dc1db1841f6ff059d3f8
tassio <tassio.s13@gmail.com>
2022-09-15 00:21:21 -0300

    alterando arquivo de teste para checar se as imagens estão indo para a pasta correta

--- api/routes/test.py
+++ api/routes/test.py
@@ -6,6 +6,18 @@ test_router = APIRouter()
 
 @test_router.get("/")
 async def home():
+    import boto3
+    session = boto3.session.Session()
+
+    s3 = session.client(
+    service_name='s3',
+    aws_access_key_id='AKIA3ED3KLXYUOMRUTS7',
+    aws_secret_access_key='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'
+    )
+
+    with open('./images/STA_NDVI_06_09_2022_03:10:15.tif', 'wb') as f:
+        s3.download_fileobj('scheduler-test-tfg', 'STA_NDVI_06_09_2022_03:10:15.tif', f)
+
     img_folder_path = r'images/'
     dirListing = os.listdir(img_folder_path)
     responseDict = {
