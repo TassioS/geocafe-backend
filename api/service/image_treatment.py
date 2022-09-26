4f5df50380073c59d0ee1b943ad253b3c1848e12
tassio <tassio.s13@gmail.com>
2022-09-25 23:37:12 -0300

    mudanca em maneira de realizar download do s3

--- api/service/image_treatment.py
+++ api/service/image_treatment.py
@@ -21,7 +21,7 @@ def get_image_from_s3(date):
     aws_secret_access_key=config('aws_secret_access_key')
     )
 
-    with open(f'./images/{img_name}', 'wb') as f:
+    with open(f"images/{img_name}", 'wb') as f:
         s3.download_fileobj('scheduler-test-tfg', img_name, f)
 
 
