1db625283f3f0ade9b8b2367709c2928c32e4a68
tassio <tassio.s13@gmail.com>
2022-10-23 08:43:27 -0300

    alteracao para o script rodar apenas após o intervalo de 17 dias depois da imagem mais recente

--- scriptGee.py
+++ scriptGee.py
@@ -2,78 +2,89 @@
 #importando libs
 import ee
 from geemap import Map, ee_export_image
-
-# %%
-#Inicializando o gee
-service_account = 'testscheduler@tfg-gee-scheduler.iam.gserviceaccount.com'
-credentials = ee.ServiceAccountCredentials(service_account, 'google-credentials.json')
-ee.Initialize(credentials)
-
-# %%
-#Carregando limites do municipio de santo Antonio
-roi = ee.FeatureCollection('projects/tfg-gee-scheduler/assets/limiteSantoAntonio')
-
-# %%
-#Centralizando o mapa na longitude/latitude/zoom desejados
-mapObject = Map(center=[-20.95,-44.92],zoom=11)
-
-# %%
-#Obtendo as imagens da coleção
-img = (ee.ImageCollection('LANDSAT/LC08/C01/T1_RT_TOA')
-.filterMetadata('CLOUD_COVER', 'less_than', 50)
-.filterDate('2019-11-01', '2020-11-30')
-.mosaic()
-.clip(roi))
-
-# %%
-#Função NDVI
-ndvi = img.normalizedDifference(['B5', 'B4'])
-#Expressão NDVI QUAD
-ndviQUAD = img.expression(
-  ' - 8.712 + (17.325 * ndvi) - (8.739 * ndvi**2) ',
-  {
-    'ndvi': ndvi,    
-  })
-
-# %%
-#Adicionando Layer ao mapa de vizualização
-paleta = ['FFFFFF', 'CE7E45', 'DF923D', 'F1B555', 'FCD163', '99B718',
-               '74A901', '66A000', '529400', '3E8601', '207401', '056201',
-               '004C00', '023B01', '012E01', '011D01', '011301']
-vis_params1 = {
-  'min': -10,
-  'max': 1,
-  'palette': paleta
-}
-mapObject.addLayer(ndviQUAD, vis_params1, 'NDVI')
-
-# %%
-#Adicionando segundo Layer
-vis_params2 = {
-  'bands': ['B5', 'B4', 'B2'],
-  'max': 1,
-  'gamma': 1
-}
-mapObject.addLayer(img, vis_params2)
-
-# %%
 from datetime import datetime
-imgName = 'STA_NDVI_date.tif'
-now = datetime.now()
-now_string = now.strftime("%d_%m_%Y")
-imgName = imgName.replace('date',now_string)
-pathImg = './images/'+imgName
 
-import boto3
-session = boto3.session.Session()
-from decouple import config
-s3 = session.client(
-    service_name='s3',
-    aws_access_key_id=config('aws_access_key_id'),
-    aws_secret_access_key=config('aws_secret_access_key')
-)
+from api.service.map import retrieve_dates
 
-ee_export_image(img, pathImg, scale=90, crs=None, region=roi.geometry(), file_per_band=False)
+def getTifImage(start_date):
+  #Inicializando o gee
+  service_account = 'testscheduler@tfg-gee-scheduler.iam.gserviceaccount.com'
+  credentials = ee.ServiceAccountCredentials(service_account, 'google-credentials.json')
+  ee.Initialize(credentials)
 
-with open(pathImg, "rb") as f:
-    s3.upload_fileobj(f, 'scheduler-test-tfg', imgName)
+  
+  #Carregando limites do municipio de santo Antonio
+  roi = ee.FeatureCollection('projects/tfg-gee-scheduler/assets/limiteSantoAntonio')
+
+
+  #Centralizando o mapa na longitude/latitude/zoom desejados
+  mapObject = Map(center=[-20.95,-44.92],zoom=11)
+
+
+  #Obtendo as imagens da coleção
+  img = (ee.ImageCollection('LANDSAT/LC08/C02/T1_RT_TOA')
+  .filterMetadata('CLOUD_COVER', 'less_than', 50)
+  .filterDate(start_date)
+  .mosaic()
+  .clip(roi))
+
+
+  #Função NDVI
+  ndvi = img.normalizedDifference(['B5', 'B4'])
+  #Expressão NDVI QUAD
+  ndviQUAD = img.expression(
+    ' - 8.712 + (17.325 * ndvi) - (8.739 * ndvi**2) ',
+    {
+      'ndvi': ndvi,    
+    })
+
+
+  #Adicionando Layer ao mapa de vizualização
+  paleta = ['FFFFFF', 'CE7E45', 'DF923D', 'F1B555', 'FCD163', '99B718',
+                '74A901', '66A000', '529400', '3E8601', '207401', '056201',
+                '004C00', '023B01', '012E01', '011D01', '011301']
+  vis_params1 = {
+    'min': -10,
+    'max': 1,
+    'palette': paleta
+  }
+  mapObject.addLayer(ndviQUAD, vis_params1, 'NDVI')
+
+
+  #Adicionando segundo Layer
+  vis_params2 = {
+    'bands': ['B5', 'B4', 'B2'],
+    'max': 1,
+    'gamma': 1
+  }
+  mapObject.addLayer(img, vis_params2)
+
+
+
+  imgName = 'STA_NDVI_date.tif'
+  now = datetime.now()
+  now_string = now.strftime("%d_%m_%Y")
+  imgName = imgName.replace('date',now_string)
+  pathImg = './images/'+imgName
+
+  import boto3
+  session = boto3.session.Session()
+  from decouple import config
+  s3 = session.client(
+      service_name='s3',
+      aws_access_key_id=config('aws_access_key_id'),
+      aws_secret_access_key=config('aws_secret_access_key')
+  )
+
+  ee_export_image(img, pathImg, scale=90, crs=None, region=roi.geometry(), file_per_band=False)
+
+  with open(pathImg, "rb") as f:
+      s3.upload_fileobj(f, 'scheduler-test-tfg', imgName)
+
+dates = retrieve_dates()
+today = datetime.today()
+lastImg = max(dates)
+dayDiff = today - lastImg
+print(dayDiff)
+if(dayDiff.days >= 17):
+  print(getTifImage(today.strftime("%Y-%m-%d")))
